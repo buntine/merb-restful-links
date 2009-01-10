87240460a1223c7df49c0101fa515979e134c80a
Andy Buntine <info@andrewbuntine.com>
2009-01-10 23:47:48 +1100

    Added a bunch of specs

--- spec/merb-restful-links_spec.rb
+++ spec/merb-restful-links_spec.rb
@@ -2,9 +2,10 @@ require 'rubygems'
 require 'spec'
 require 'merb-core'
 require 'merb-more'
-require File.dirname(__FILE__) + '/../lib/merb-restful-links'
 
 include Merb::AssetsMixin
+
+require File.dirname(__FILE__) + '/../lib/merb-restful-links'
 
 describe "merb-restful-links" do
 
@@ -23,5 +24,36 @@ describe "merb-restful-links" do
 
   end
 
+  describe "link_to functioning with method option" do
+
+    it "should accept :delete or :put as option value" do
+      lambda { link_to("Nuclear Death", "/nuclear/death", :method => :delete) }.should_not raise_error
+      lambda { link_to("Nuclear Death", "/nuclear/death", :method => :put) }.should_not raise_error
+    end
+
+    it "should not accept any other values" do
+      lambda { link_to("Nuclear Death", "/nuclear/death", :method => :cheese) }.should raise_error(ArgumentError)
+      lambda { link_to("Nuclear Death", "/nuclear/death", :method => "destruktorr") }.should raise_error(ArgumentError)
+    end
+
+    it "should generate onclick code for anchor with :put" do
+      link_to("Temple of the Underworld", "/temple/1", :method => :put).should match(/<a onclick=".+" href="\/temple\/1">Temple of the Underworld<\/a>/)
+    end
+
+    it "should generate onclick code for anchor with :delete" do
+      link_to("Temple of the Underworld", "/temple/1", :method => :delete).should match(/<a onclick=".+" href="\/temple\/1">Temple of the Underworld<\/a>/)
+    end
+
+    it "should also allow other traditional options" do
+      link_to("Time for Crime", "/crime", :method => :delete, :id => "crime_link").should match(/<a onclick=".+" id="crime_link" href="\/crime">Time for Crime<\/a>/)
+    end
+
+    it "should append onclick code to existing value instead of overwriting it"
+
+    it "should generate correct onclick with PUT"
+
+    it "should generate correct onclick with DELETE"
+
+  end
 
 end
